6db1e757a71eeda932593c12caefb6de570f25b1
Khayot Razzakov <hrazzoqov0002@gmail.com>
2024-08-12 00:38:18 +0500

    kel audio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weddingDate = new Date('September 19, 2024 06:43:00').getTime();
+const weddingDate = new Date('September 19, 2024 17:00:00').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
